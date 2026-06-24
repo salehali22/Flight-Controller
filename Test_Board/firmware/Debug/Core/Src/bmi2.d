c12a6dcebf0bedd4e24bc8cb5b816f1e60631836
Saleh Ali Ahmed Alhomeidy <salehalhomeidy@gmail.com>
2026-06-24 14:59:48 +0400

    Remove testboard folder from repo

--- Test_Board/firmware/Debug/Core/Src/bmi2.d
+++
@@ -1,4 +0,0 @@
-Core/Src/bmi2.o: ../Core/Src/bmi2.c ../Core/Inc/bmi2.h \
- ../Core/Inc/bmi2_defs.h
-../Core/Inc/bmi2.h:
-../Core/Inc/bmi2_defs.h:
--- Test_Board/firmware/Debug/Core/Src/bmi270.d
+++
@@ -1,5 +0,0 @@
-Core/Src/bmi270.o: ../Core/Src/bmi270.c ../Core/Inc/bmi270.h \
- ../Core/Inc/bmi2.h ../Core/Inc/bmi2_defs.h
-../Core/Inc/bmi270.h:
-../Core/Inc/bmi2.h:
-../Core/Inc/bmi2_defs.h:
